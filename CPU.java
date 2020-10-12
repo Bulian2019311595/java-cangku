a15484cfe12e7710fde7c48329b17c2f239fe014
Bulian2019311595 <72741901+Bulian2019311595@users.noreply.github.com>
2020-10-12 20:19:52 +0800

    Add files via upload

---
+++ CPU.java
@@ -0,0 +1,12 @@
+package pack1;
+
+class CPU {
+	 int speed;
+	 int getSpeed(){
+	  return speed;
+	 }
+	 public void setSpeed(int speed){
+	  this.speed=speed;
+	 }
+
+	}
---
+++ HardDisk.java
@@ -0,0 +1,12 @@
+package pack1;
+
+class HardDisk {
+	 int amount;
+	 int getAmount(){
+	  return amount;
+	 }
+	 public void setAmount(int amount){
+	  this.amount=amount;
+	 }
+
+	}
